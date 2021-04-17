38d253b771c46ae10ed5b84e73afb7a9f684cc38
Michael Sanchez <michael.sanchez.aguirre@ibm.com>
2021-04-17 10:41:48 -0500

    Se agrega proyecto base

---
+++ config/firebase.ts
@@ -0,0 +1,10 @@
+export const config = {
+    apiKey: "AIzaSyBc96CgB93bXvboaEttjxiYRfBsImPVo_s",
+    authDomain: "merqueo-app.firebaseapp.com",
+    databaseURL: "https://merqueo-app-default-rtdb.firebaseio.com",
+    projectId: "merqueo-app",
+    storageBucket: "merqueo-app.appspot.com",
+    messagingSenderId: "977150222069",
+    appId: "1:977150222069:web:9f424d13a14001b10f6fbc",
+    measurementId: "G-FG771K0FEQ"
+};
